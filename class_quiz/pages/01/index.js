c82a97dcb40625f8e7203bde109d51c0a6315492
sunnwave <ssun20886@gmail.com>
2024-09-27 05:27:23 +0900

    section04 quiz

--- class_quiz/pages/01/index.js
+++ class_quiz/pages/01/index.js
@@ -1,5 +1,138 @@
-import {} from "../../styles/01-index.js";
+import {
+  Body,
+  MenuWrapper,
+  NavContainer,
+  NavLabel,
+  NavWrapper,
+  NavIcon,
+  ProfileButton,
+  ProfileImage,
+  ProfileLabel,
+  ProfileName,
+  ProfileWrapper,
+  QuestionContainer,
+  QuestionNumber,
+  QuestionTitle,
+  QuestionTitleWrapper,
+  QuestionToggle,
+  QuestionWrapper,
+  SearchIcon,
+  SearchWrapper,
+  MenuItem,
+  SearchIconImg,
+  ToggleIconImg,
+  MoreInfoIconImg,
+  Hr,
+} from "../../styles/01-index.js";
 
 export default function FaqPage() {
-  return <div></div>;
+  return (
+    <>
+      <Body>
+        <SearchWrapper>
+          <SearchIcon>
+            <SearchIconImg src={"search_icon.png"} />
+          </SearchIcon>
+        </SearchWrapper>
+        <ProfileWrapper>
+          <ProfileLabel>마이</ProfileLabel>
+          <ProfileImage src={"profile_image.png"} />
+          <ProfileName>임정아</ProfileName>
+          <ProfileButton>
+            <MoreInfoIconImg src={"arrow2_icon.png"} />
+          </ProfileButton>
+        </ProfileWrapper>
+        <MenuWrapper>
+          <MenuItem>공지사항</MenuItem>
+          <MenuItem>이벤트</MenuItem>
+          <MenuItem style={{ color: "#FF1B6D" }}>
+            FAQ
+            <hr
+              style={{ background: "#FF1B6D", height: "2px", border: "0" }}
+            ></hr>
+          </MenuItem>
+          <MenuItem>Q&A</MenuItem>
+        </MenuWrapper>
+        <Hr></Hr>
+        <QuestionContainer>
+          <QuestionWrapper>
+            <QuestionTitleWrapper>
+              <QuestionNumber>Q.01</QuestionNumber>
+              <QuestionTitle>리뷰 작성은 어떻게 하나요?</QuestionTitle>
+            </QuestionTitleWrapper>
+            <QuestionToggle>
+              <ToggleIconImg src={"arrow_icon.png"} />
+            </QuestionToggle>
+          </QuestionWrapper>
+          <QuestionWrapper>
+            <QuestionTitleWrapper>
+              <QuestionNumber>Q.02</QuestionNumber>
+              <QuestionTitle>리뷰 수정/삭제는 어떻게 하나요?</QuestionTitle>
+            </QuestionTitleWrapper>
+            <QuestionToggle>
+              <ToggleIconImg src={"arrow_icon.png"} />
+            </QuestionToggle>
+          </QuestionWrapper>
+          <QuestionWrapper>
+            <QuestionTitleWrapper>
+              <QuestionNumber>Q.03</QuestionNumber>
+              <QuestionTitle>아이디/비밀번호를 잊어버렸어요</QuestionTitle>
+            </QuestionTitleWrapper>
+            <QuestionToggle>
+              <ToggleIconImg src={"arrow_icon.png"} />
+            </QuestionToggle>
+          </QuestionWrapper>
+          <QuestionWrapper>
+            <QuestionTitleWrapper>
+              <QuestionNumber>Q.04</QuestionNumber>
+              <QuestionTitle>회원탈퇴를 하고싶어요.</QuestionTitle>
+            </QuestionTitleWrapper>
+            <QuestionToggle>
+              <ToggleIconImg src={"arrow_icon.png"} />
+            </QuestionToggle>
+          </QuestionWrapper>
+          <QuestionWrapper>
+            <QuestionTitleWrapper>
+              <QuestionNumber>Q.05</QuestionNumber>
+              <QuestionTitle>출발지 설정은 어떻게 하나요?</QuestionTitle>
+            </QuestionTitleWrapper>
+            <QuestionToggle>
+              <ToggleIconImg src={"arrow_icon.png"} />
+            </QuestionToggle>
+          </QuestionWrapper>
+          <QuestionWrapper>
+            <QuestionTitleWrapper>
+              <QuestionNumber>Q.06</QuestionNumber>
+              <QuestionTitle>비밀번호를 변경하고 싶어요</QuestionTitle>
+            </QuestionTitleWrapper>
+            <QuestionToggle>
+              <ToggleIconImg src={"arrow_icon.png"} />
+            </QuestionToggle>
+          </QuestionWrapper>
+        </QuestionContainer>
+        <Hr></Hr>
+        <NavContainer>
+          <NavWrapper>
+            <NavIcon src={"home_icon.png"} />
+            <NavLabel>홈</NavLabel>
+          </NavWrapper>
+          <NavWrapper>
+            <NavIcon
+              src={"location_icon.png"}
+              style={{ width: "35px", height: "40px" }}
+            />
+            <NavLabel>잇츠로드</NavLabel>
+          </NavWrapper>
+          <NavWrapper>
+            <NavIcon src={"wish_icon.png"} />
+            <NavLabel>마이찜</NavLabel>
+          </NavWrapper>
+          <NavWrapper>
+            <NavIcon src={"my_icon.png"} />
+            <NavLabel style={{ color: "#FF1B6D" }}>마이</NavLabel>
+          </NavWrapper>
+        </NavContainer>
+      </Body>
+    </>
+  );
 }
--- class_quiz/styles/01-index.js
+++ class_quiz/styles/01-index.js
@@ -1,11 +1,151 @@
 import styled from "@emotion/styled";
 
-export const Search = styled.div``;
-export const Menu = styled.div``;
+export const Body = styled.div`
+  width: 640px;
+  height: 1064px;
+  position: absolute;
+  top: 200px;
+  left: 200px;
+  border: 1px solid gray;
+  padding: 50px;
+`;
+export const SearchWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+export const SearchIcon = styled.button`
+  width: 23px;
+  height: 23px;
+  border: none;
+  background-color: white;
+`;
 
-export const MyProfile = styled.div``;
+export const SearchIconImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
 
-export const QuestionContainer = styled.div``;
-export const QuestionWrapper = styled.div``;
+export const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const ProfileLabel = styled.label`
+  font-family: Noto Sans KR;
+  font-size: 40px;
+  font-weight: 700;
+  text-align: center;
+`;
+export const ProfileImage = styled.img`
+  width: 60px;
+  height: 60px;
+  margin-left: 250px;
+`;
+export const ProfileName = styled.label`
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-weight: 700;
+  text-align: center;
+  margin-left: 19px;
+`;
+export const ProfileButton = styled.button`
+  margin-left: 2px;
+`;
 
-export const Navigator = styled.div``;
+export const MoreInfoIconImg = styled.img``;
+
+export const Hr = styled.hr`
+  transform: translateX(-50px);
+  width: 640px;
+`;
+export const MenuWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 40px;
+  margin-bottom: 58px;
+`;
+export const MenuItem = styled.div`
+  font-family: Noto Sans KR;
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 43.44px;
+  text-align: center;
+  color: #cacaca;
+`;
+
+export const QuestionContainer = styled.div`
+  width: 100%;
+  height: 695px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 25px;
+  padding-bottom: 54px;
+`;
+export const QuestionWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const QuestionTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const QuestionNumber = styled.label`
+  font-family: Noto Sans KR;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 26.06px;
+  text-align: left;
+  color: #adadad;
+`;
+export const QuestionTitle = styled.label`
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 34.75px;
+  text-align: left;
+  color: #000000;
+  margin-top: 5px;
+`;
+export const QuestionToggle = styled.button`
+  /* width: 22.63px;
+  height: 22.63px; */
+`;
+export const ToggleIconImg = styled.img``;
+export const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 12px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 96px;
+`;
+export const NavWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 55px;
+  height: 40px;
+`;
+export const NavIcon = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+export const NavLabel = styled.label`
+  font-family: Noto Sans KR;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 23.17px;
+  text-align: center;
+  color: #adadad;
+  width: 82px;
+  margin-top: 10px;
+`;
