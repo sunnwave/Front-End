783c96c47d99d27698c5c1731b5a3df5ff0dbd52
sunnwave <ssun20886@gmail.com>
2024-10-15 21:42:31 +0900

    section11 | map Quiz-1

---
+++ class_quiz/pages/07/products/index.js
@@ -0,0 +1,9 @@
+import FetchProducts from "../../../src/components/units/product/fetch/FetchProducts.container";
+
+export default function FetchProductsPage() {
+  return (
+    <>
+      <FetchProducts />
+    </>
+  );
+}
