8807b94c138a4576156b1f14eab437f2659ae1d4
sunnwave <ssun20886@gmail.com>
2024-10-15 16:59:41 +0900

    section08-class | map 보충

--- class/pages/section08/08-03-map-boards-delete/index.js
+++ class/pages/section08/08-03-map-boards-delete/index.js
@@ -39,11 +39,12 @@ export default function StaticRoutingMovePage() {
     <div>
       {data?.fetchBoards.map((el, index) => (
         //1.프레그먼트란? <></>, <Fragment></Fragment>
-        //2.프레그먼트에 key를 입력하는 방법 =><Fragment key={i}></Fragment>
+        //2.프레그먼트에 key를 입력하는 방법 => <Fragment key={i}></Fragment>
         //3. 특별한 이유가 없으면? Fragment로 감싸자. <div></div>는 1개 더 그려야돼서 조금 느려짐
 
         <div key={el.number}>
-          {/*index는 게시글을 삭제할 때, 다음 게시글이 올라오면서 기존 index와 동일한 값을 갖게 됨. 즉, 유일한 값이 아님*/}
+          {/* key를 부여함으로 인해 각각 고유한 태그임을 알려주게 된다. 키 값은 중복되지 않는 고유한 값을 주어야 한다.*/}
+          {/* key값에 index값을 부여하게 되면 index는 게시글을 삭제할 때, 다음 게시글이 올라오면서 기존 index와 동일한 값을 갖게 됨. 즉, 유일한 값이 아님*/}
           <span>
             <input type="checkbox" />
           </span>
