e3b54d28e558308f9d29de56fa90dc08ff5d259f
sunnwave <ssun20886@gmail.com>
2025-08-02 19:51:17 +0900

    section13 quiz modal address

---
+++ class_quiz/pages/section13/13-07-modal-address-library/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import DaumPostcodeEmbed, { Address } from "react-daum-postcode";
+import { on } from "events";
+
+export default function App(): JSX.Element {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [address, setAddress] = useState("");
+
+  const onToggleModal = (): void => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const handleComplete = (data: Address): void => {
+    setAddress(data.address);
+    onToggleModal();
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={onToggleModal}>
+        Open Modal
+      </Button>
+      {isModalOpen && (
+        <Modal
+          title="주소 검색"
+          closable={{ "aria-label": "Custom Close Button" }}
+          open={isModalOpen}
+          onOk={onToggleModal}
+          onCancel={onToggleModal}
+        >
+          <DaumPostcodeEmbed onComplete={handleComplete}></DaumPostcodeEmbed>
+        </Modal>
+      )}
+      {address && (
+        <div>
+          <h3>Selected Address</h3>
+          <p>{address}</p>
+        </div>
+      )}
+    </>
+  );
+}
