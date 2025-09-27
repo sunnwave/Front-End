b9d018afe1bc59d6021ad393d5676cdbd868db67
sunnwave <ssun20886@gmail.com>
2025-09-27 14:46:07 +0900

    pagination

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -6,7 +6,7 @@ interface IApolloSettingProps {
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
   const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(), //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시로 저장해 놓기
   });
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
