372904d74ba8586f490f0b7ae7866756aa9fb7be
sunnwave <ssun20886@gmail.com>
2026-01-23 14:37:23 +0900

    section31-03-memoization-with-map

---
+++ class/pages/section31/31-03-memoization-with-map/02-child.tsx
@@ -0,0 +1,12 @@
+import { memo } from "react";
+
+interface IWordProps {
+  el: string;
+}
+
+function Word(props: IWordProps): JSX.Element {
+  console.log("자식이 렌더링됩니다.", props.el);
+  return <span>{props.el}</span>;
+}
+
+export default memo(Word);
